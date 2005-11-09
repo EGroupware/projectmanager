aec95e82ae9a86f9d7d14280d24d360b72bea54b
Ralf Becker <ralfbecker@outdoor-training.de>
2005-11-09 20:56:55 +0000

    updated dependencies to 1.2 api and etemplate

--- setup/setup.inc.php
+++ setup/setup.inc.php
@@ -49,16 +49,11 @@ $setup_info['projectmanager']['hooks']['search_link'] = 'projectmanager.boprojec
 /* Dependencies for this app to work */
 $setup_info['projectmanager']['depends'][] = array(
 	 'appname' => 'phpgwapi',
-	 'versions' => Array('1.0.0','1.0.1')
+	 'versions' => Array('1.0.0','1.0.1','1.2')
 );
 $setup_info['projectmanager']['depends'][] = array(
 	 'appname' => 'etemplate',
-	 'versions' => Array('1.0.0','1.0.1')
-);
-// as long as the link class is not in the API
-$setup_info['projectmanager']['depends'][] = array(
-	 'appname' => 'infolog',
-	 'versions' => Array('1.0.0','1.0.1')
+	 'versions' => Array('1.0.0','1.0.1','1.2')
 );
 
 
