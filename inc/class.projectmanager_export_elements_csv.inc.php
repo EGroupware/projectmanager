642355efe95d7a600f3000caa4c8adb06deb0758
Nathan Gray <nathangray.bsc@gmail.com>
2010-11-04 16:34:58 +0000

    Change date formatting back to whatever the current user happens to have.

--- inc/class.projectmanager_export_elements_csv.inc.php
+++ inc/class.projectmanager_export_elements_csv.inc.php
@@ -155,10 +155,13 @@ class projectmanager_export_elements_csv implements importexport_iface_export_pl
 			}
 		}
 		foreach(self::$types['date-time'] as $name) {
-			if ($record->$name) $record->$name = date('Y-m-d H:i:s',$record->$name); // Standard date format
+			//if ($record->$name) $record->$name = date('Y-m-d H:i:s',$record->$name); // Standard date format
+			if ($record->$name) $record->$name = date($GLOBALS['egw_info']['user']['preferences']['common']['dateformat'] . ' '.
+				($GLOBALS['egw_info']['user']['preferences']['common']['timeformat'] == '24' ? 'H' : 'h').':m:s',$record->$name); // User date format
 		}
 		foreach(self::$types['date'] as $name) {
-			if ($record->$name) $record->$name = date('Y-m-d',$record->$name); // Standard date format
+			//if ($record->$name) $record->$name = date('Y-m-d',$record->$name); // Standard date format
+			if ($record->$name) $record->$name = date($GLOBALS['egw_info']['user']['preferences']['common']['dateformat'], $record->$name); // User date format
 		}
 
 		foreach(array('pe_used_time', 'pe_planned_time', 'pe_replanned_time') as $duration) {
