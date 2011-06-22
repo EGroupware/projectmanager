08109cc54e192f2d1a3f78736dd44462bf10715d
Nathan Gray <nathangray.bsc@gmail.com>
2011-06-22 22:10:57 +0000

    Experimental new gantt chart

---
+++ inc/class.projectmanager_gantt.inc.php
@@ -0,0 +1,132 @@
+<?php
+
+class projectmanager_gantt extends projectmanager_elements_bo {
+
+	public $public_functions = array(
+		'chart'	=> true,
+		'ajax_gantt_project' => true,
+	);
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function chart() {
+		if (isset($_REQUEST['pm_id']))
+                {
+                        $pm_id = $_REQUEST['pm_id'];
+                        $GLOBALS['egw']->session->appsession('pm_id','projectmanager',$pm_id);
+                }
+                else
+                {
+                        $pm_id = $GLOBALS['egw']->session->appsession('pm_id','projectmanager');
+                }
+		if(!$pm_id) 
+		{
+			egw::redirect_link('/index.php', array(
+                                'menuaction' => 'projectmanager.projectmanager_ui.index',
+                                'msg'        => lang('You need to select a project first'),
+                        ));
+		}
+		
+		egw_framework::validate_file('dojo/dojo','dojo');
+		egw_framework::validate_file('.','gantt','projectmanager');
+		egw_framework::includeCSS('/phpgwapi/js/dojo/dojo/resources/dojo.css');
+		egw_framework::includeCSS('/phpgwapi/js/dojo/dijit/themes/claro/claro.css');
+		egw_framework::includeCSS('/phpgwapi/js/dojo/dojox/gantt/resources/gantt.css');
+		$content = '<script type="text/javascript">var gantt_project_ids = ' . json_encode($pm_id) . ';</script>';
+		$content .= '<div class="ganttContent"><div id="gantt"></div></div>';
+		$GLOBALS['egw']->framework->render($content, 'Test', true);
+	}
+
+	public function ajax_gantt_project($project_id) {
+		if(!is_array($project_id)) {
+			$project_id = explode(',',$project_id);
+		}
+		$projects = array();
+		foreach($project_id as $pm_id) {
+			$projects[] = $this->add_project($pm_id);
+		}
+		$response = egw_json_response::get();
+		$response->data($projects);
+	}
+
+	// Get the data into required format
+	protected function add_project($pm_id) {
+		if ($pm_id != $this->project->data['pm_id'])
+		{
+			if (!$this->project->read($pm_id) || !$this->project->check_acl(EGW_ACL_READ))
+			{
+				return;
+			}
+		}
+		$project = $this->project->data + array(
+			'name'	=>	egw_link::title('projectmanager', $this->project->data['pm_id']),
+		);
+		if(is_array($project['pm_members'])) {
+			foreach($project['pm_members'] as $uid => &$member_data) {
+				$member_data['name'] = common::grab_owner_name($member_data['member_uid']);
+			}
+		}
+		$project['elements'] = $this->add_elements($pm_id);
+		return $project;
+	}
+
+	protected function add_elements($pm_id) {
+		$elements = array();
+		$filter = array(
+			'pm_id'	=> $pm_id
+		);
+		// defining start- and end-times depending on $params['planned_times'] and the availible data
+		foreach(array('start','end') as $var)
+		{
+			if ($params['planned_times'])
+			{
+				$$var = "CASE WHEN pe_planned_$var IS NULL THEN pe_real_$var ELSE pe_planned_$var END";
+			}
+			else
+			{
+				$$var = "CASE WHEN pe_real_$var IS NULL THEN pe_planned_$var ELSE pe_real_$var END";
+			}
+		}
+		$extra_cols = array(
+			$start.' AS pe_start',
+			$end.' AS pe_end',
+		);
+
+		$element_index = array();
+		foreach((array) $this->search(array(),false,'pe_start,pe_end',$extra_cols,
+                        '',false,'AND',false,$filter) as $pe)
+                {
+                        //echo "$line: ".print_r($pe,true)."<br>\n";
+                        if (!$pe) continue;
+
+			if($pe['pe_app'] == 'projectmanager') {
+				$project = $this->add_project($pe['pe_app_id']);
+				if($project) $elements[] = $project;
+			} else {
+				$pe['pe_start'] = (int)$pe['pe_start'];
+				$pe['duration'] = max(($pe['pe_end'] - $pe['pe_start']) / 3600 / 8, 0);
+			
+				$elements[] = $pe;
+			}
+			$element_index[$pe['pe_id']] = $pe;
+		}
+		// adding the constraints
+		foreach($elements as &$pe) {
+			foreach((array)$this->constraints->search(array('pm_id'=>$pm_id, 'pe_id'=>$pe['pe_id'])) as $constraint)
+			{
+				if($pe['pe_id'] == $constraint['pe_id_end']) continue;
+				$next_id = $constraint['pe_id_end'];
+				// Chart requires constraints to respect dates
+				if($pe['pe_start'] > $element_index[$next_id]['pe_start'])
+				{
+				//	$pe['pe_constraint'][] = $next_id;
+				}
+				$pe['pe_constraint'][] = $next_id;
+			}
+		}
+//_debug_array($elements);
+		return $elements;
+	}
+}
+?>
---
+++ js/gantt.js
@@ -0,0 +1,87 @@
+/**
+ * JS for active gantt chart
+ */
+
+dojo.require('dojox.gantt.GanttChart');
+dojo.ready(function(){
+var ganttChart = new dojox.gantt.GanttChart({
+  readOnly: true,                    //optional: determine if gantt chart is editable
+  dataFilePath: "gantt_default.json", //optional: json data file path for load and save, default is "gantt_default.json"
+  height: $('#divSubContainer').height(),                        //optional: chart height in pixel, default is 400px
+  width: $('#divAppbox').width()-150,                        //optional: chart width in pixel, default is 600px
+  withResource: true                  //optional: display the resource chart or not
+}, "gantt");                          //"gantt" is the node container id of gantt chart widget
+
+
+// Fetch data
+var req = new egw_json_request('projectmanager.projectmanager_gantt.ajax_gantt_project', [gantt_project_ids]);
+req.sendRequest(true, ganttLoadProject, ganttChart);
+});
+
+function ganttLoadProject(data) {
+console.log(data);
+	for(var i = 0; i < data.length; i++) {
+		var project = ganttAddProject(this, data[i]);
+		
+		this.addProject(project);
+	}
+	this.init();
+}
+
+function ganttAddProject(graph, data) {
+	var project = new dojox.gantt.GanttProjectItem({
+		id: data.pm_id,
+		name: data.name,
+		startDate: new Date(data.pm_real_start*1000 || data.pm_planned_start*1000 || new Date().getTime())
+	});
+	for(var j = 0; j < data.elements.length; j++) {
+		if(data.elements[j].pe_id == undefined) {
+			graph.addProject(ganttAddProject(graph,data.elements[j]));
+		} else {
+			var task = ganttAddElement(data,data.elements[j]);
+			// Gantt fails if dates don't work out
+			if(task.startTime < project.startDate) project.startDate = task.startTime;
+			project.addTask(task);
+		}
+	}
+	return project;
+}
+function ganttAddElement(project_data, data) {
+
+	var task_data = {
+		id: data.pe_id,
+		name: data.pe_title,
+		startTime: new Date(data.pe_start*1000),
+		duration: data.duration || 1,
+		percentage: data.pe_completion ? parseInt(data.pe_completion.substr(0,data.pe_completion.length-1)) : 0,
+	};
+	// Chart can only handle single, correct constraints
+	if(data.pe_constraint) {
+		var previous;
+		for(var i = 0; i < data.pe_constraint.length; i++) {
+			for(var j = 0; j < project_data.elements.length; j++) {
+				if(project_data.elements[j].pe_id == data.pe_constraint[i]) {
+					previous = project_data.elements[j];
+					if(previous.pe_start < data.pe_start) {
+						task_data.previousTaskId = data.pe_constraint[i];
+						break;
+					} else {
+						console.warn('Constraint does not match times. ' + data.pe_title + ' after ' + previous.pe_title);
+						break;
+					}
+				}
+			}
+		}
+	}
+	// Chart can only handle one owner / task
+	if(data.pe_resources && project_data.pm_members) {
+		for(var i = 0; i < data.pe_resources.length; i++) {
+			if(project_data.pm_members[data.pe_resources[i]]) {
+				task_data.taskOwner = project_data.pm_members[data.pe_resources[i]].name;
+				break;
+			}
+		}
+	}
+	var task = new dojox.gantt.GanttTaskItem(task_data);
+	return task;
+}
