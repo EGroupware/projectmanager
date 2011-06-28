9e89482eb04e9b2c981093ca35f1221b1dd09ac2
Nathan Gray <nathangray.bsc@gmail.com>
2011-06-28 16:02:27 +0000

    Admins don't get limited

--- inc/class.projectmanager_merge.inc.php
+++ inc/class.projectmanager_merge.inc.php
@@ -652,7 +652,7 @@ class projectmanager_merge extends bo_merge
 			$query = array('pm_id' => $this->pm_id);
 			if($this->elements) $query['pe_id'] = $this->elements;
 
-			if($this->export_limit) {
+			if($this->export_limit && !$GLOBALS['egw_info']['user']['apps']['admin']) {
 				$limit = array(0,(int)$this->export_limit);
 				// Need to do this to give an error
 				$count = count($this->projectmanager_elements_bo->search($query));
