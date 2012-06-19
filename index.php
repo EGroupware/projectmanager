3cb4ee201a67d684290fda4a0ac197bde41b99e3
Ralf Becker <ralfbecker@outdoor-training.de>
2012-06-19 17:06:53 +0000

    run 1.9.001 update via datasource constructor, and NOT director user to setup

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * @link http://www.egroupware.org
  * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
  * @package projectmanager
- * @copyright (c) 2005-9 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @copyright (c) 2005-12 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @version $Id$
  */
@@ -21,12 +21,21 @@ include('../header.inc.php');
 include_once(EGW_INCLUDE_ROOT.'/projectmanager/setup/setup.inc.php');
 if ($setup_info['projectmanager']['version'] != $GLOBALS['egw_info']['apps']['projectmanager']['version'])
 {
-	$GLOBALS['egw']->common->egw_header();
-	parse_navbar();
-	echo '<p style="text-align: center; color:red; font-weight: bold;">'.lang('Your database is NOT up to date (%1 vs. %2), please run %3setup%4 to update your database.',
-		$setup_info['projectmanager']['version'],$GLOBALS['egw_info']['apps']['projectmanager']['version'],
-		'<a href="../setup/">','</a>')."</p>\n";
-	$GLOBALS['egw']->common->egw_exit();
+	// 1.9.001 update is run inside EGroupware by datasource constructor
+	if ($GLOBALS['egw_info']['apps']['projectmanager']['version'] === '1.9.001')
+	{
+		include_once(EGW_INCLUDE_ROOT.'/projectmanager/inc/class.datasource.inc.php');
+		new datasource();
+	}
+	else
+	{
+		common::egw_header();
+		parse_navbar();
+		echo '<p style="text-align: center; color:red; font-weight: bold;">'.lang('Your database is NOT up to date (%1 vs. %2), please run %3setup%4 to update your database.',
+			$setup_info['projectmanager']['version'],$GLOBALS['egw_info']['apps']['projectmanager']['version'],
+			'<a href="../setup/">','</a>')."</p>\n";
+		common::egw_exit();
+	}
 }
 unset($setup_info);
 
@@ -35,4 +44,4 @@ $pm_id = $GLOBALS['egw']->session->appsession('pm_id','projectmanager');
 $GLOBALS['egw']->redirect_link('/index.php',array(
 	'menuaction' => $pm_id ? 'projectmanager.projectmanager_elements_ui.index' : 'projectmanager.projectmanager_ui.index',
 ));
-$GLOBALS['egw']->common->egw_exit();
+common::egw_exit();
