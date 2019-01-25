d897aca7c3c4cd2b361f889850ca506492086703
Hadi Nategh <hnategh@gmail.com>
2019-01-25 12:42:05 +0100

    Deprecate CKEDITOR and remove it from source

--- inc/class.projectmanager_hooks.inc.php
+++ inc/class.projectmanager_hooks.inc.php
@@ -228,9 +228,7 @@ class projectmanager_hooks
 			$file = Array(
 				'Site configuration' => Egw::link('/index.php',array(
 					'menuaction' => 'projectmanager.projectmanager_admin.config',
-					// As long as CKEditor needs CSP exceptions, this needs to
-					// load in an iframe
-					'ajax' => 'false',
+					'ajax' => 'true',
 				)),
 				'Custom fields' => Egw::link('/index.php','menuaction=admin.admin_customfields.index&appname=projectmanager&use_private=1&ajax=true'),
 				'Global Categories'  => Egw::link('/index.php',array(
