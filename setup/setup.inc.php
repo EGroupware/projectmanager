3b056fed7eab7e189bd92c6fd7c8c5f88d656951
Ralf Becker <RalfBecker@outdoor-training.de>
2021-05-08 14:51:00 +0200

    Bump version to 21.1

--- setup/setup.inc.php
+++ setup/setup.inc.php
@@ -11,7 +11,7 @@
  */
 
 $setup_info['projectmanager']['name']      = 'projectmanager';
-$setup_info['projectmanager']['version']   = '20.1';
+$setup_info['projectmanager']['version']   = '21.1';
 $setup_info['projectmanager']['app_order'] = 5;
 $setup_info['projectmanager']['tables']    = array('egw_pm_projects','egw_pm_extra','egw_pm_elements','egw_pm_constraints','egw_pm_milestones','egw_pm_roles','egw_pm_members','egw_pm_pricelist','egw_pm_prices','egw_pm_eroles');
 $setup_info['projectmanager']['enable']    = 1;
@@ -28,10 +28,7 @@ $setup_info['projectmanager']['license']  = 'GPL';
 $setup_info['projectmanager']['description'] =
 'The projectmanager is a complete rewrite of the projects app using modern object orientated programming
 technics and a widget based user-interface (eTemplate). It has a better integration in eGroupWare
-by using other already existing apps like InfoLog, Calendar or the TimeSheet.<br>
-For more information see the <a href="http://outdoor-training.de/pdf/projects-rewrite.pdf"
-target="_blank">concept of the rewrite</a> (<a href="http://outdoor-training.de/pdf/Neuprogrammierung-Projektmanagement.pdf"
-target="_blank">german version</a>).';
+by using other already existing apps like InfoLog, Calendar or the TimeSheet.';
 $setup_info['projectmanager']['note'] =
 'It was sponsored by:<ul>
 <li> <a href="http://www.blanke-textil.de" target="_blank">Fritz Blanke GmbH & Co.KG</a></li>
@@ -54,5 +51,5 @@ $setup_info['projectmanager']['hooks']['deleteaccount'] = 'projectmanager.projec
 /* Dependencies for this app to work */
 $setup_info['projectmanager']['depends'][] = array(
 	 'appname' => 'api',
-	 'versions' => Array('20.1')
+	 'versions' => Array('21.1')
 );
